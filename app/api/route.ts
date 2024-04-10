19d34a8b189b5b7f7d36aa534dedbfbda7cf8eef
Paweł Ciosek <ciosek.pawel@gmail.com>
2024-04-10 23:00:29 +0200

    Update .gitignore and add Cloudflare API integration

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,10 +1,33 @@
-import { cookies } from "next/headers";
+import { cookies, headers } from "next/headers";
 import { NextRequest } from "next/server";
 
-export const runtime = 'edge';
+export const runtime = "edge";
 
-export const GET = (request: NextRequest) => {
+export const GET = async (request: NextRequest) => {
   const cook = cookies();
   const time = new Date().getTime();
-  return new Response(`Hello, ${cook.get("name") || "world"}! ${time}`);
+  const cloudfrareKey = process.env.CLOUDFLARE_API_KEY;
+  const cfAccountId = process.env.CLOUDFLARE_ACCOUNT_ID;
+
+  const req = await fetch(
+    `https://api.cloudflare.com/client/v4/accounts/${cfAccountId}/ai/run/@cf/meta/llama-2-7b-chat-fp16`,
+    {
+      method: "POST",
+      body: JSON.stringify({
+        max_tokens: 256,
+        messages: [
+          {
+            role: "user",
+            content: "Hello!",
+          },
+        ],
+      }),
+      headers: {
+        Authorization: `Bearer ${cloudfrareKey}`,
+      },
+    }
+  );
+  const res = await req.json();
+
+  return Response.json(res);
 };
