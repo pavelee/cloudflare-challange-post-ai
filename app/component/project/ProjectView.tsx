ab6d791e60d52ac5e4329850cf78143fbe8dfa57
Paweł Ciosek <ciosek.pawel@gmail.com>
2024-04-14 13:47:40 +0200

    Refactor ProjectView component layout and add save button

--- app/component/project/ProjectView.tsx
+++ app/component/project/ProjectView.tsx
@@ -24,13 +24,6 @@ const Editor = (
     const { content, setContent, saveProject, isSaving } = props;
     return (
         <div className="space-y-4">
-            <Button
-                loading={isSaving}
-                type="primary"
-                onClick={saveProject}
-            >
-                Save
-            </Button>
             <Input.TextArea
                 value={content}
                 rows={100}
@@ -79,34 +72,51 @@ export const ProjectView = (
     }
 
     return (
-        <div className="container mx-auto w-1/2">
-            <div className="space-y-4">
-                <div className="flex justify-center gap-4">
-                    <Switch
-                        checkedChildren="Edit"
-                        unCheckedChildren="View"
-                        defaultChecked={false}
-                        onChange={(checked) => setIsEdit(checked)}
-                    />
-                    <Switch
-                        checkedChildren="Switch to view after save"
-                        unCheckedChildren="Stay in edit mode after save"
-                        defaultChecked={true}
-                        onChange={(checked) => setSwitchToPreviewAfterSave(checked)}
-                    />
-                </div>
-                {
-                    isEdit ? (
-                        <Editor
-                            content={content}
-                            setContent={setContent}
-                            saveProject={saveProject}
-                            isSaving={isSaving}
+        <div className="container mx-auto">
+            <div className="space-y-4 flex gap-2">
+                <div className="flex flex-col gap-4 w-1/6">
+                    <div className="sticky top-0 flex flex-col gap-4 p-4">
+                        <Switch
+                            checkedChildren="Edit"
+                            unCheckedChildren="View"
+                            defaultChecked={false}
+                            value={isEdit}
+                            onChange={(checked) => setIsEdit(checked)}
                         />
-                    ) : (
-                        <Preview content={content} />
-                    )
-                }
+                        <Switch
+                            checkedChildren="Switch to view after save"
+                            unCheckedChildren="Stay in edit mode after save"
+                            defaultChecked={true}
+                            value={switchToPreviewAfterSave}
+                            onChange={(checked) => setSwitchToPreviewAfterSave(checked)}
+                        />
+                        {
+                            isEdit && (
+                                <Button
+                                    loading={isSaving}
+                                    type="default"
+                                    onClick={saveProject}
+                                >
+                                    Save
+                                </Button>
+                            )
+                        }
+                    </div>
+                </div>
+                <div className="w-5/6">
+                    {
+                        isEdit ? (
+                            <Editor
+                                content={content}
+                                setContent={setContent}
+                                saveProject={saveProject}
+                                isSaving={isSaving}
+                            />
+                        ) : (
+                            <Preview content={content} />
+                        )
+                    }
+                </div>
             </div>
         </div>
     )
