fc75b7d41816ba576d08e1c70a4796320ddee6e6
Paweł Ciosek <ciosek.pawel@gmail.com>
2024-04-14 10:12:41 +0200

    Update ProjectFactory to include messages when creating a Project object

--- app/_factory/ProjectFactory.ts
+++ app/_factory/ProjectFactory.ts
@@ -1,7 +1,12 @@
 import { Project, ProjectType } from "../_model/Project";
+import { MessageFactory } from "./MessageFactory";
 
 export class ProjectFactory {
   static createFromObject(data: ProjectType): Project {
-    return new Project(data.id, data.sourceCode);
+    return new Project(
+      data.id, 
+      data.sourceCode, 
+      data.messages.map((message) => MessageFactory.createFromObject(message))
+    );
   }
 }
