c6e67492ff13afd8d1bf2c0fabe8575b7cc20bdb
Paweł Ciosek <ciosek.pawel@gmail.com>
2024-04-14 18:14:26 +0200

    Remove console.log statement in summary route

--- app/api/project/[project_id]/summary/route.ts
+++ app/api/project/[project_id]/summary/route.ts
@@ -6,7 +6,6 @@ export const runtime = 'edge';
 export const POST = async (request: NextRequest) => {
   const { content } = await request.json();
   const summary = await summaryContent(content);
-  console.log(summary);
   return NextResponse.json({
     summary,
   });
