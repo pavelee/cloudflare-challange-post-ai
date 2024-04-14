26bae502703d3892176f323e739df5e57011d073
Paweł Ciosek <ciosek.pawel@gmail.com>
2024-04-14 20:16:04 +0200

    Refactor generateProjectID function to hash project ID in generateProjectID.ts

--- app/util/generateProjectID.ts
+++ app/util/generateProjectID.ts
@@ -4,5 +4,6 @@ export const generateProjectID = (projectName: string) => {
   const salt = generateSalt();
   const cleanedProjectName = projectName.replace(/[^a-zA-Z0-9]/g, "");
   const projectID = `${cleanedProjectName}-${salt}`;
-  return projectID;
+  const hasedProjectID = Buffer.from(projectID).toString("base64");
+  return hasedProjectID;
 };
